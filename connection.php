f5739410cf9b290847928faa610caac360fc1253
Gayuh Laksono <gayuhlksn2@gmail.com>
2024-10-25 19:53:04 +0700

    Redo

--- connection.php
+++ connection.php
@@ -13,7 +13,7 @@ $name = "sig_map4";
 
 $connection = mysqli_connect($host, $user, $pass, $name);
 if (mysqli_connect_errno()) {
-    echo "connection database mysqli gagal!!! : " . mysqli_connect_error();
+    echo "connectionx database mysqli gagal!!! : " . mysqli_connect_error();
 }
 
 //mysqli_select_db($name, $koneksi) or die("Tidak ada database yang dipilih!");
