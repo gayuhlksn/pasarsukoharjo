b094f8bdc97b25b7100a754f060f4da613f7817b
Gayuh Laksono <gayuhlksn2@gmail.com>
2024-10-25 17:03:30 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,6 +4,13 @@ $user = "userpasar";
 $pass = "gayuhlksn";
 $name = "sig_map4";
 
+// local
+// $host = "localhost";
+// $user = "root";
+// $pass = "";
+// $name = "sig_map4";
+
+
 $connection = mysqli_connect($host, $user, $pass, $name);
 if (mysqli_connect_errno()) {
     echo "connectionx database mysqli gagal!!! : " . mysqli_connect_error();
