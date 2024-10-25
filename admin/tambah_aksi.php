10b223a47dbc7a1b9663deaa448f37335c512a7b
Gayuh Laksono <gayuhlksn2@gmail.com>
2024-10-25 16:57:36 +0700

    update tambah aksi (hapus gambar)

--- admin/tambah_aksi.php
+++ admin/tambah_aksi.php
@@ -22,34 +22,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $latitude = mysqli_real_escape_string($connection, $_POST['latitude']);
     $longitude = mysqli_real_escape_string($connection, $_POST['longitude']);
 
-    // Proses upload gambar
-    if (isset($_FILES['gambar']) && $_FILES['gambar']['error'] == UPLOAD_ERR_OK) {
-        $uploadDir = 'uploads/';
-        $uploadFile = $uploadDir . basename($_FILES['gambar']['name']);
-        $fileType = strtolower(pathinfo($uploadFile, PATHINFO_EXTENSION));
-
-        // Validasi tipe file gambar
-        $allowedTypes = array('jpg', 'jpeg', 'png', 'gif');
-        if (in_array($fileType, $allowedTypes)) {
-            // Pindahkan file ke direktori uploads
-            if (move_uploaded_file($_FILES['gambar']['tmp_name'], $uploadFile)) {
-                echo "File valid dan berhasil diupload.\n";
-            } else {
-                echo "File tidak dapat diupload.\n";
-                exit();
-            }
-        } else {
-            echo "Tipe file tidak diizinkan. Hanya JPG, JPEG, PNG, dan GIF yang diperbolehkan.\n";
-            exit();
-        }
-    } else {
-        echo "Tidak ada file yang diupload atau terjadi kesalahan.\n";
-        exit();
-    }
-
     // Simpan data ke database
-    $query = "INSERT INTO pasar (nama_pasar, alamat, deskripsi, jumlah_kios_dan_los, jam_operasional, gambar, latitude, longitude) 
-              VALUES ('$nama_pasar', '$alamat', '$deskripsi', '$jumlah_kios_dan_los', '$jam_operasional', '".basename($_FILES['gambar']['name'])."', '$latitude', '$longitude')";
+    $query = "INSERT INTO pasar (nama_pasar, alamat, deskripsi, jumlah_kios_dan_los, jam_operasional, latitude, longitude) 
+              VALUES ('$nama_pasar', '$alamat', '$deskripsi', '$jumlah_kios_dan_los', '$jam_operasional', '$latitude', '$longitude')";
 
     if (mysqli_query($connection, $query)) {
         echo "Data berhasil disimpan.";
@@ -59,4 +34,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     mysqli_close($connection);
 }
-?>
