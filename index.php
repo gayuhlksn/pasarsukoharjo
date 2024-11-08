82f8cd8c4ad8fb084c545c53af54976b54155f6a
Gayuh Laksono <gayuhlksn2@gmail.com>
2024-11-09 02:07:36 +0700

    update index (lagi)

--- index.php
+++ index.php
@@ -461,11 +461,11 @@ $PATH = "http://localhost/pasarsukoharjo/";
 									</div>
 								</div>
 							</div>
-							<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+							<a class="carousel-control-prev" href="Sebelumnya" role="button" data-slide="prev">
 								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 								<span class="sr-only">Previous</span>
 							</a>
-							<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+							<a class="carousel-control-next" href="Selanjutnya" role="button" data-slide="next">
 								<span class="carousel-control-next-icon" aria-hidden="true"></span>
 								<span class="sr-only">Next</span>
 							</a>
